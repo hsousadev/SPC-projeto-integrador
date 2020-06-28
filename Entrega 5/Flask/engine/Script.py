82da8952c605b9cba903099db03336e856dbc36d
Matheus Campos <matheus.campos14@fatec.sp.gov.br>
2020-06-28 12:09:03 -0300

    Update Script.py

--- Entrega 5/Flask/engine/Script.py
+++ Entrega 5/Flask/engine/Script.py
@@ -1,17 +1,11 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
 import pandas as pd
 
-
-pessoa_fisica = pd.read_csv("../Dados/Complementares/fatec_pessoa_fisica.csv", sep = '|', encoding='UTF-8', header = 1)
-endereco_pessoa_fisica = pd.read_csv("../Dados/Complementares/fatec_endereco_pessoa_fisica.csv", sep = '|', encoding='UTF-8', header = 1)
-fatec_operacao = pd.read_csv("../Dados/Complementares/fatec_opr.csv", sep = '|', encoding='UTF-8', header = 1)
-fatec_movimento = pd.read_csv("../Dados/Complementares/fatec_mvt.csv", sep = '|', encoding='UTF-8', header = 1)
-fatec_pagamento = pd.read_csv("../Dados/Complementares/fatec_pgt.csv", sep = '|', encoding='UTF-8', header = 1)
-
-
-
+'''
+pessoa_fisica = pd.read_csv("/Dados/Complementares/fatec_pessoa_fisica.cvs", sep = '|', encoding='UTF-8', header = 1)
+endereco_pessoa_fisica = pd.read_csv("/Dados/Complementares/fatec_endereco_pessoa_fisica.csv", sep = '|', encoding='UTF-8', header = 1)
+fatec_operacao = pd.read_csv("/Dados/Complementares/fatec_opr.csv", sep = '|', encoding='UTF-8', header = 1)
+fatec_movimento = pd.read_csv("/Dados/Complementares/fatec_mvt.csv", sep = '|', encoding='UTF-8', header = 1)
+fatec_pagamento = pd.read_csv("/Dados/Complementares/fatec_pgt.csv", sep = '|', encoding='UTF-8', header = 1)
 
 pessoa_fisica_colunas = pessoa_fisica.columns[1:len(pessoa_fisica.columns) - 1]
 endereco_pessoa_fisica_colunas = endereco_pessoa_fisica.columns[1:len(endereco_pessoa_fisica.columns) - 1]
@@ -25,7 +19,6 @@ coluna_fatec_operacao = fatec_operacao.columns[1]
 coluna_fatec_movimento = fatec_movimento.columns[1]
 coluna_fatec_pagamento = fatec_pagamento.columns[1]
 
-
 pessoa_fisica = pd.DataFrame(pessoa_fisica, columns = pessoa_fisica_colunas)
 endereco_pessoa_fisica = pd.DataFrame(endereco_pessoa_fisica, columns = endereco_pessoa_fisica_colunas)
 fatec_operacao = pd.DataFrame(fatec_operacao, columns = fatec_operacao_colunas)
@@ -36,7 +29,6 @@ endereco_pessoa_fisica.columns = ['id_pessoa_fisica', 'nom_cidade', 'des_estado'
 fatec_operacao.columns = ['id_opr_cad_pos', 'doc_cli', 'tip_cli', 'qtd_pcl', 'dat_vct_ult_pcl', 'vlr_ctrd_fta_tfm', 'sdo_ddr_tfm', 'vlr_ctrd', 'id_ult_rss_opr', 'id_mdl', 'cod_mdl', 'id_fnt']
 fatec_movimento.columns = ['id_opr_cad_pos', 'id_mvt_cad_pos', 'dat_vct', 'qtd_pcl_vnc', 'qtd_pcl_pgr', 'vlr_tot_fat_tfm',	'vlr_min_fat_tfm', 'vlr_pcl_tfm', 'tip_mvt', 'prd']
 fatec_pagamento.columns = ['id_opr_cad_pos', 'id_mvt_cad_pos', 'id_pgt_cad_pos', 'dat_pgt', 'dat_vct_tfm', 'vlr_pgt_tfm', 'cod_mdl', 'cmd']
-
 
 pessoa_fisica = pessoa_fisica[(pd.isna(pessoa_fisica['cpf']) != True)]
 endereco_pessoa_fisica = endereco_pessoa_fisica[(pd.isna(endereco_pessoa_fisica['id_pessoa_fisica']) != True)]
@@ -50,6 +42,7 @@ fatec_operacao = fatec_operacao[(fatec_operacao['id_opr_cad_pos'] != coluna_fate
 fatec_movimento = fatec_movimento[(fatec_movimento['id_opr_cad_pos'] != coluna_fatec_movimento)]
 fatec_pagamento = fatec_pagamento[(fatec_pagamento['id_opr_cad_pos'] != coluna_fatec_pagamento)]
 
+'''
 
-
+fatec_operacao_alterados = pd.read_excel("../dados/alterados/fatec_operacao.xlsx")
 
