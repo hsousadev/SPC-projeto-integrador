d46fe5c68cf141ec1ad686314c5c001f2df365ed
Matheus Campos <matheus.campos14@fatec.sp.gov.br>
2020-06-28 11:02:37 -0300

    Flask

--- Entrega 5/Flask/main.py
+++ Entrega 5/Flask/main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__, template_folder='templates')
 
-
 @app.route('/')
 def main():
     return redirect(url_for('screen1')) # Chama a função screen1 (Para sempre redirecionar para a tela de login)
