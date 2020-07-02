99b2df28d7a4b144858427522b32dc5a83619be2
Matheus Campos <matheus.campos14@fatec.sp.gov.br>
2020-07-01 23:31:17 -0300

    Confiabilidade

--- Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/confiabilidade.py
+++ Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/confiabilidade.py
@@ -12,10 +12,10 @@ def valida_fonte():
 
 
 def validaCpf(cpf, d1=0, d2=0, i=0):
-    while i < 10:
-        d1, d2, i = (
-            d1+(int(cpf[i])(11-i-1))) % 11 if i < 9 else d1, (d2+(int(cpf[i])(11-i))) % 11, i+1
-    return (int(cpf[9]) == (11-d1 if d1 > 1 else 0)) and (int(cpf[10]) == (11-d2 if d2 > 1 else 0))
+    while i<10:
+        d1,d2,i=(d1+(int(cpf[i])*(11-i-1)))%11 if i<9 else d1,(d2+(int(cpf[i])*(11-i)))%11,i+1
+    return (int(cpf[9])==(11-d1 if d1>1 else 0)) and (int(cpf[10])==(11-d2 if d2>1 else 0))
+
 
 
 def cpfInvalidos(fonte):
