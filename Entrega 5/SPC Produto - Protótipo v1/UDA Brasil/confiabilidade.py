2aa67be5082cced078c1716abc2e7589ccec3a65
Matheus Campos <matheus.campos14@fatec.sp.gov.br>
2020-07-03 09:05:30 -0300

    Atualizando arquivos

--- Entrega 5/SPC Produto - Protótipo v1/UDA Brasil/confiabilidade.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-from data import fatec_operacao, fatec_fonte, indice_fontes
-
-
-# for fonte in fatec_fonte:
--- Entrega 5/SPC Produto - Protótipo v1/UDA Brasil/consistencia.py
+++ Entrega 5/SPC Produto - Protótipo v1/UDA Brasil/consistencia.py
@@ -1,19 +1,21 @@
-import pandas as pd
-from data import fatec_operacao, fatec_modalidade, indice_fontes
+import pandas as pd 
 
-lista_modalidade = list(fatec_modalidade["COD_MDL"])
+fatec_operacao = pd.read_excel("dados/alterados/fatec_opr.xlsx")
+modalidade = pd.read_excel("dados/principais/STG_MDL.xlsx")
 
+lista_modalidade = list(modalidade["COD_MDL"])
 
 def consistencia_opr(fonte):
-    campos_inconsistentes = fatec_operacao.query(
-        f"cod_mdl not in {lista_modalidade}")['cod_mdl'].count()
+    campos_inconsistentes = fatec_operacao.query(f"cod_mdl not in {lista_modalidade}")['cod_mdl'].count()
     return ((campos_inconsistentes / fatec_operacao.shape[0]) * 100)
 
 
 def consistencia_modalidade_operacao():
+    indice_fontes = set(list(int(float(str(i).strip())) for i in list(fatec_operacao['id_fnt'])))
     matriz_consistencia = list()
     for fonte in indice_fontes:
         porcentagem = consistencia_opr(fonte)
         matriz_consistencia.append([fonte, porcentagem])
     matriz_consistencia.sort()
     return matriz_consistencia
+
