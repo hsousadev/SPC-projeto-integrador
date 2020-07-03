62c1ee2b2886e7853a5324950f87f5f4320c0e7c
Matheus Campos <matheus.campos14@fatec.sp.gov.br>
2020-07-03 08:30:12 -0300

    Confiabilidade.py atualizada

--- Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/confiabilidade.py
+++ Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/confiabilidade.py
@@ -16,15 +16,30 @@ def validaCpf(cpf, d1=0, d2=0, i=0):
         d1,d2,i=(d1+(int(cpf[i])*(11-i-1)))%11 if i<9 else d1,(d2+(int(cpf[i])*(11-i)))%11,i+1
     return (int(cpf[9])==(11-d1 if d1>1 else 0)) and (int(cpf[10])==(11-d2 if d2>1 else 0))
 
+def validaCnpj():
+    pass
 
 
 def cpfInvalidos(fonte):
     cpf_invalidos = list()
-    cpf_fonte = fatec_operacao[(fatec_operacao["id_fnt"] == fonte)]
-    listCpfs = list(cpf_fonte["doc_cli"])
-    for cpf in listCpfs:
-        if not validaCpf(cpf):
-            cpf_invalidos.append(cpf)
+    cnpj_invalidos = list()
+    doc_fonte = fatec_operacao[(fatec_operacao["id_fnt"] == fonte)]
+    listDoc = [list(doc_fonte["doc_cli"]), list(doc_fonte["tip_cli"])]
+    
+    for index in range(len(listDoc)):
+        if listDoc[1][index] == "F":
+            if not validaCpf(listDoc[0][index]):
+                cpf_invalidos.append(listDoc[0][index])
+        
+        else:
+            if not validaCnpj(listDoc[0][index]):
+                cnpj_invalidos.append(listDoc[0][index])
+    
 
-    porcentagem = (len(cpf_invalidos) / len(listCpfs)) * 100
+    porcentagem = ((len(cpf_invalidos) + len(cnpj_invalidos)) / len(listDoc)) * 100
     return porcentagem, cpf_invalidos
+
+
+
+
+
