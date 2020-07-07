e3469adecbfc21a683c390c796e1e9b48e223cba
José Danrley <jose.silva478@fatec.sp.gov.br>
2020-07-07 19:08:31 -0300

    Remoções de cache e arquivos temporários realizada

--- Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/consistencia.py
+++ Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/consistencia.py
@@ -18,9 +18,11 @@ def consistencia_modalidade_operacao():
     matriz_consistencia.sort()
     return matriz_consistencia
 
-## TABELA MOVIMENTO
+# TABELA MOVIMENTO
 
-def consistencia_id(fonte): # LOGICA PRECISA SER ATUALIZADA, NÃO ESTA DE ACORDO COM AS REGRAS DE NEGOCIO. (LISTA DE ID's) O numero de ID'S INVALIDOS VAI SER MAIOR QUE O REAL.  
+
+# LOGICA PRECISA SER ATUALIZADA, NÃO ESTA DE ACORDO COM AS REGRAS DE NEGOCIO. (LISTA DE ID's) O numero de ID'S INVALIDOS VAI SER MAIOR QUE O REAL.
+def consistencia_id(fonte):
     id_invalidos = list()
     dataframe = fatec_operacao[(fatec_operacao["id_fnt"] == fonte)]
     lista_id = list(fatec_movimento['id_opr_cad_pos'])
@@ -28,7 +30,6 @@ def consistencia_id(fonte): # LOGICA PRECISA SER ATUALIZADA, NÃO ESTA DE ACORDO
         if index not in list(dataframe['id_opr_cad_pos']):
             id_invalidos.append(index)
     return (len(id_invalidos) / dataframe.shape[0]) * 100
-    
 
 
 def consistencia_id_operacao():
@@ -38,17 +39,3 @@ def consistencia_id_operacao():
         matriz_consistencia.append(fonte, porcentagem)
     matriz_consistencia.sort()
     return matriz_consistencia
-
-
-
-
-
-'''
-def verifica_data():
-    dataframe = fatec_movimento[(fatec_movimento[''])]
-
-def consistencia_dat_vct():
-    matriz_consistencia = list()
-    for fonte in indice_fontes:
-        porcentagem = 
-'''
