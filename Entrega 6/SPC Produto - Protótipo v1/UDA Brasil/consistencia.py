32abffee9832a1ceaf2f2a7a66e5f4d8e82e66e2
Matheus Campos <matheus.campos14@fatec.sp.gov.br>
2020-07-08 00:47:16 -0300

    Consistencia

--- Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/consistencia.py
+++ Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/consistencia.py
@@ -25,12 +25,13 @@ def consistencia_modalidade_operacao():
 def consistencia_id(fonte):
     id_invalidos = list()
     dataframe = fatec_operacao[(fatec_operacao["id_fnt"] == fonte)]
-    lista_id = list(fatec_movimento['id_opr_cad_pos'])
+    lista_id = list(dataframe['id_opr_cad_pos'])
     for index in lista_id:
         if index not in list(dataframe['id_opr_cad_pos']):
             id_invalidos.append(index)
     return (len(id_invalidos) / dataframe.shape[0]) * 100
 
+# criar um zip das listas id_opr e id_fnt da tabela operação. Comparar com a lista id_opr da tabela movimento e identificar a fonte.
 
 def consistencia_id_operacao():
     matriz_consistencia = list()
--- Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/teste.py
+++ Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/teste.py
@@ -2,12 +2,22 @@ import pandas as pd
 from data import fatec_operacao, fatec_movimento, fatec_pagamento, modalidade, indice_fontes
 
 
+def consistencia_id(fonte):
+    id_invalidos = list()
+    dataframe = fatec_operacao[(fatec_operacao["id_fnt"] == fonte)]
+    print(dataframe)
+    lista_id = list(dataframe['id_opr_cad_pos'])
+    for index in lista_id:
+        if index not in list(dataframe['id_opr_cad_pos']):
+            id_invalidos.append(index)
+    return (len(id_invalidos) / dataframe.shape[0]) * 100
 
-def teste():
-    teste = list()
-    for ID in list(fatec_movimento['id_opr_cad_pos']):
-        if ID in list(fatec_operacao['id_opr_cad_pos']):
-            teste.append(ID)
-    return len(teste)
 
+def consistencia_id_operacao():
+    matriz_consistencia = list()
+    for fonte in indice_fontes:
+        porcentagem = consistencia_id(fonte)
+        matriz_consistencia.append(fonte, porcentagem)
+    matriz_consistencia.sort()
+    return matriz_consistencia
 
