e906b6482d08486ee3d43b122cede45e0b12af6e
José Danrley <45850297+JDanrley@users.noreply.github.com>
2020-07-08 01:07:01 -0300

    Delete teste.py

--- Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/teste.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from data import fatec_operacao, fatec_movimento, fatec_pagamento, modalidade, indice_fontes
-
-def teste():
-    teste = list()
-    for ID in list(fatec_movimento['id_opr_cad_pos']):
-        if ID in list(fatec_operacao['id_opr_cad_pos']):
-            teste.append(ID)
-    return len(teste)
