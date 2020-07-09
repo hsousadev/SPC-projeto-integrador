badfc4515e45cd5b300aa087f35d8595e0ff372b
Matheus Campos <matheus.campos14@fatec.sp.gov.br>
2020-07-08 22:55:36 -0300

    Consistencia e Data atualizados

--- Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/consistencia.py
+++ Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/consistencia.py
@@ -3,7 +3,6 @@ from data import fatec_operacao, fatec_movimento, fatec_pagamento, modalidade, i
 
 lista_modalidade = list(modalidade["COD_MDL"])
 
-
 def consistencia_opr(fonte):
     campos_inconsistentes = fatec_operacao.query(
         f"cod_mdl not in {lista_modalidade}")['cod_mdl'].count()
@@ -20,21 +19,12 @@ def consistencia_modalidade_operacao():
 
 # TABELA MOVIMENTO
 
-
-# LOGICA PRECISA SER ATUALIZADA, NÃO ESTA DE ACORDO COM AS REGRAS DE NEGOCIO. (LISTA DE ID's) O numero de ID'S INVALIDOS VAI SER MAIOR QUE O REAL.
 def consistencia_id(fonte):
-    id_invalidos = list()
-    dataframe = fatec_operacao[(fatec_operacao["id_fnt"] == fonte)]
-    lista_id = zip(list(dataframe['id_opr_cad_pos']), list(dataframe['id_fnt']))
-    for index in list(fatec_movimento['id_opr_cad_pos']):
-        if index not in lista_id:
-            id_invalidos.append(index)
-    return (len(id_invalidos) / dataframe.shape[0]) * 100
+    idsInvalidos = list()
+    for 
 
 
 
-
-# criar um zip das listas id_opr e id_fnt da tabela operação. Comparar com a lista id_opr da tabela movimento e identificar a fonte.
 
 def consistencia_id_operacao():
     matriz_consistencia = list()
