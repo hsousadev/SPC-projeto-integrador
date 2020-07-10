4d7b79a99eee845a848cc287fad813acd527bc00
Matheus Campos <matheus.campos14@fatec.sp.gov.br>
2020-07-10 19:46:30 -0300

    Operação e Movimento

--- Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/movimento.py
+++ Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/movimento.py
@@ -32,6 +32,40 @@ def datasInvalidas_movimento(fonte):
     return porcentagem, datas_invalidas
 
 
+
+def verifica_valor(df, coluna, fonte):
+    campos_invalidos = list()
+    
+    for index in zip(df[coluna], df['id_fnt']):
+        if index[1] == fonte:
+            try:
+                float(index[0])
+                
+            except ValueError:
+                if index[0] != 'NULL':
+                    campos_invalidos.append(index[0])
+    
+    porcentagem = 100 - (len(campos_invalidos) / len(df[coluna])) * 100
+
+    return porcentagem, campos_invalidos
+
+
+def validaNumerico(df, colunas):
+    matriz = list([fonte] for fonte in indice_fontes)
+
+    for index in range(len(matriz)):
+        campos_inconsistentes = list()
+
+        for coluna in colunas:
+            porcentagem = verifica_valor(df, coluna, matriz[index][0])
+            campos_inconsistentes.append(porcentagem[0])
+    
+        porcentagem = sum(campos_inconsistentes) / len(colunas)
+        matriz[index].append(porcentagem)
+        campos_inconsistentes.clear()
+    return matriz
+
+
 '''
 CONSISTÊNCIA
 '''
--- Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/teste.py
+++ Entrega 6/SPC Produto - Protótipo v1/UDA Brasil/teste.py
@@ -1,6 +1,39 @@
 import pandas as pd
 from data import fatec_operacao, fatec_movimento, fatec_pagamento, modalidade, indice_fontes
 
-for index in list(fatec_movimento['id_opr_cad_pos']):
-  print(index)
-              
+
+def verifica_valor(df, coluna, fonte):
+    campos_invalidos = list()
+    
+    for index in zip(df[coluna], df['id_fnt']):
+        if index[1] == fonte:
+            try:
+                float(index[0])
+                
+            except ValueError:
+                if index[0] != 'NULL':
+                    campos_invalidos.append(index[0])
+    
+    porcentagem = 100 - (len(campos_invalidos) / len(df[coluna])) * 100
+
+    return porcentagem, campos_invalidos
+
+
+
+
+def validaNumerico(df, colunas):
+    matriz = list([fonte] for fonte in indice_fontes)
+
+    for index in range(len(matriz)):
+        campos_inconsistentes = list()
+
+        for coluna in colunas:
+            porcentagem = verifica_valor(df, coluna, matriz[index][0])
+            campos_inconsistentes.append(porcentagem[0])
+    
+        porcentagem = sum(campos_inconsistentes) / len(colunas)
+
+        matriz[index].append(porcentagem)
+        campos_inconsistentes.clear()
+    return matriz
+
